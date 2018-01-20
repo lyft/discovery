4c2589f3996a997df0c99d32a6982dbaa3fac063
patrickboland4 <patrickboland4@gmail.com>
2018-01-20 07:34:59 -0600

    cleaned up plugins/. added changed plugins entry on .gitignore file

--- plugins/config.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-discovery_parent_dir = '/'.join(os.getcwd().split('/')[:-2])
-sys.path.append(discovery_parent_dir)
-
--- plugins/temp.py
+++
@@ -1,8 +0,0 @@
-#  import os, sys
-import pdb
-#  sys.path.append(os.path.dirname('/home/patrickos/discovery/discovery'))
-from pprint import pprint
-#  pprint(sys.path)
-pdb.set_trace()
-from discovery.app.settings import values
-print values
