ea60fe74944c8fc883dd39caeb32b3abb8d51270
Lita Cho <lcho@lyft.com>
2018-10-08 15:55:03 -0700

    update

--- app/models/host.py
+++ app/models/host.py
@@ -10,6 +10,7 @@ class CustomPynamoSession(requests.Session):
     super(CustomPynamoSession, self).__init__()
     self.mount('http://', adapters.HTTPAdapter(pool_maxsize=settings.value.CONNECTION_POOL_SIZE))
 
+
 session_cls = CustomPynamoSession
 
 
